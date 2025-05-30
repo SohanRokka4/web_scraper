b3059557aeab480366e14ca6a49b77faffbecba0
Sohan Rokka <sohanrokka@192.168.1.13>
2025-05-30 11:23:55 +0545

    so i used the req handler and dis dis

---
+++ proj-server/server.py
@@ -0,0 +1,27 @@
+import BaseHTTPServer
+
+class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+    '''handle HTTP requests by returning a fixed 'page'.'''
+
+    # Page to send back,
+    Page = '''\
+<html>
+<body>
+<p>Hello, web!</p>
+</body>
+</html>
+'''
+
+    # Handle a GET request.
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-Type","text/html")
+        self.send_header("Content-Length",str(len(slef.page)))
+        self.end_headers()
+        self.wfile.write(self.page)
+
+
+if __name__ == '__main__':
+    serverAddress = ('',8080)
+    server = BaseHTTPServer.HTTPServeer(serverAddress, RequestHandler)
+    server.server_forever()
