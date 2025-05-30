f58fb06632305ab017083845bde1fa41e9ae24b1
Sohan Rokka <sohanrokka@192.168.1.13>
2025-05-30 12:09:38 +0545

    so i fixed a bunch of stuff

--- proj-server/server.py
+++ proj-server/server.py
@@ -1,6 +1,6 @@
-import BaseHTTPServer
+from http.server import HTTPServer, BaseHTTPRequestHandler
 
-class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+class RequestHandler(BaseHTTPRequestHandler):  # Fixed: inherit from BaseHTTPRequestHandler
     '''handle HTTP requests by returning a fixed 'page'.'''
 
     # Page to send back,
@@ -15,13 +15,13 @@ class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     # Handle a GET request.
     def do_GET(self):
         self.send_response(200)
-        self.send_header("Content-Type","text/html")
-        self.send_header("Content-Length",str(len(slef.page)))
+        self.send_header("Content-Type", "text/html")
+        self.send_header("Content-Length", str(len(self.Page)))  # Fixed: self.Page
         self.end_headers()
-        self.wfile.write(self.page)
+        self.wfile.write(self.Page.encode())  # Added .encode() for Python 3
 
 
 if __name__ == '__main__':
-    serverAddress = ('',8080)
-    server = BaseHTTPServer.HTTPServeer(serverAddress, RequestHandler)
-    server.server_forever()
+    serverAddress = ('', 8080)
+    server = HTTPServer(serverAddress, RequestHandler)
+    server.serve_forever()
