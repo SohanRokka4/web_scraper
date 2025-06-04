93876c24d67c5d8d5b8abeb7426ac937dccd20fb
Sohan Rokka <sohanrokka@192.168.1.13>
2025-06-04 22:10:30 +0545

    so in this thing i m gonna commit

--- proj-server/server2.py
+++ proj-server/server2.py
@@ -7,7 +7,15 @@ class request_handeler(BaseHTTPRequestHandler):
         self.send_page(page)
 
     def create_page(self):
-        # ...fill in...
+        values = {
+            'date_time'   : self.date_time_string(),
+            'client_host' : self.client_address[0],
+            'client_port' : self.client_address[1],
+            'command'     : self.command,
+            'path'        : self.path
+        }
+        page = self.Page.format(**values)
+        return page
 
     def send_page(self,page):
         # ...fill in...
